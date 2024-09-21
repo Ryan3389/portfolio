e1e8002411b5549ff5e1d6cd0c9c7d868c8f4e60
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-09-21 10:58:52 -0400

    set up home page structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import HomePage from './pages/HomePage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1>Error Page</h1>,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
