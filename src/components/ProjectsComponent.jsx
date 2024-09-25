efa2f7a82d29f52e9c9cd6b9be0f1f68922e83ed
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-09-25 12:05:10 -0400

    added images for first two projects

--- src/components/ProjectsComponent.jsx
+++ src/components/ProjectsComponent.jsx
@@ -2,15 +2,34 @@ import { useContext } from "react";
 import { DataContext } from "../utils/Context";
 import SkillsComponent from "./SkillsComponent";
 
+// const ProjectsComponent = () => {
+//     const { projectData } = useContext(DataContext);
+
+//     return (
+//         <article className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+//             {projectData.map((project, index) => (
+//                 <SkillsComponent
+//                     key={index}
+//                     heading={project.heading}
+//                     image={project.image}
+//                     description={project.description}
+//                     link={project.link}
+//                 />
+//             ))}
+//         </article>
+//     );
+// };
+
 const ProjectsComponent = () => {
     const { projectData } = useContext(DataContext);
 
     return (
-        <article className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+        <article className="m-auto grid grid-cols-1 md:grid-cols-2 gap-8">
             {projectData.map((project, index) => (
                 <SkillsComponent
                     key={index}
                     heading={project.heading}
+                    image={project.image}
                     description={project.description}
                     link={project.link}
                 />
@@ -18,5 +37,6 @@ const ProjectsComponent = () => {
         </article>
     );
 };
+
 
 export default ProjectsComponent;
