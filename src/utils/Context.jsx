5af102f47b7c15e97b73e25603117ddb8f7ed44e
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-09-25 12:36:37 -0400

    added image and description for projects

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -1,6 +1,8 @@
 import { createContext, useState } from "react";
 import weatherApp from '../../src/images/weatherApp.png'
 import blog from '../../src/images/blog.png'
+import socialNetworkApi from '../../src/images/socialNetworkApi.png'
+import sqlCompanyManager from '../../src/images/sqlCompanyManager.png'
 export const DataContext = createContext()
 
 export const DataProvider = ({ children }) => {
@@ -8,25 +10,25 @@ export const DataProvider = ({ children }) => {
         {
             heading: "Weather App",
             image: weatherApp,
-            description: "Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.",
-            link: "https://ryan3389.github.io/Weather-Forecast-App/"
+            description: "A dynamic weather application that utilizes the OpenWeather API to provide real-time weather updates and a 5-day forecast for any city. The app features search history saving, allowing users to quickly access weather details from previous searches, and offers clickable search results for instant weather information retrieval.",
+            link: "https://github.com/Ryan3389/Weather-Forecast-App",
         },
         {
             heading: "Blog",
             image: blog,
-            description: "Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.",
-            link: "https://ryan3389.github.io/Ryan-Cuthbert-Blog/"
+            description: "A responsive blog interface where users can add their username and post messages, with submissions displayed dynamically on the page. The project includes a toggle feature to switch between dark and light themes, offering a personalized user experience. Built using modern frontend technologies for a clean and intuitive design.",
+            link: 'https://github.com/Ryan3389/Ryan-Cuthbert-Blog'
         },
         {
             heading: 'SQL Company Manager',
-            image: blog,
-            description: "Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.",
+            image: sqlCompanyManager,
+            description: "A command-line application for managing a company's departments, roles, and employees using a SQL database. Users can view all departments, roles, and employees, as well as add new departments, roles, and employees. Additionally, the app allows users to update employee roles, streamlining company organization and employee management.",
             link: "https://github.com/Ryan3389/sql-company-manager"
         },
         {
             heading: 'Social Network API',
-            image: blog,
-            description: "Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.",
+            image: socialNetworkApi,
+            description: "A backend API for a social networking platform, allowing users to create profiles, post comments.",
             link: "https://github.com/Ryan3389/Social-Network-Api"
         },
     ]);
