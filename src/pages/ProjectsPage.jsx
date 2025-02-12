7546e0ccaaf3f9484e2b24c8e98b8a18d8ae9903
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-02-12 16:28:04 -0500

    updated projects

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -6,12 +6,13 @@ const ProjectsPage = () => {
             <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
                 <article className="mb-10">
                     <h2 className="text-center mb-4 text-4xl  font-extrabold">Full Stack Developer</h2>
-                    <p className="sm:text-xl text-secondaryText">Welcome to my portfolio! After completing a bootcamp focused on the MERN stack—MongoDB, Express.js, React, and Node.js—I’m excited to present the projects that showcase my skills in full-stack development. Each project reflects my ability to build dynamic, user-friendly web applications. Explore my work to see how I turn ideas into functional solutions.</p>
+                    <p className="sm:text-xl text-secondaryText">Welcome to my portfolio! I’m a full-stack developer specializing in the MERN stack—MongoDB, Express.js, React, and Node.js. I build dynamic, user-friendly web applications that turn ideas into functional solutions. Check out my work!</p>
                 </article>
                 <ProjectsComponent />
             </div>
         </section>
     )
 }
+
 
 export default ProjectsPage
--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -1,35 +1,35 @@
 import { createContext, useState } from "react";
-import movieSearch from '../../src/images/movieSearch.png'
-import speakEasy from '../../src/images/speakEasy.png'
-import socialNetworkApi from '../../src/images/socialNetworkApi.png'
-import sqlCompanyManager from '../../src/images/sqlCompanyManager.png'
+import TrackFinder from '../images/TrackFinder.png'
+import BookQuest from '../images/BookQuest.png'
+import NewsHub from '../images/NewsHub.png'
+import TastyTable from '../images/TastyTable.png'
 export const DataContext = createContext()
 
 export const DataProvider = ({ children }) => {
     const [projectData] = useState([
         {
-            heading: "PopChoice",
-            image: movieSearch,
-            description: "Welcome to PopChoice, your AI-powered movie recommendation app. Discover personalized movie suggestions in real-time, tailored to your tastes. Simply enter the type of movie you're in the mood for, and let AI find the perfect match. Dive into a world of movies, effortlessly curated just for you.",
-            link: "https://github.com/Ryan3389/movie-search",
+            heading: "TrackFinder",
+            image: TrackFinder,
+            description: "TrackFinder is a simple and efficient CRUD API that allows users to search for music using the iTunes API",
+            link: "https://github.com/Ryan3389/TrackFinder",
         },
         {
-            heading: "SpeakEasy",
-            image: speakEasy,
-            description: "Welcome to SpeakEasy, the next-generation language translation app powered by AI. Instantly translate text into multiple languages with unparalleled accuracy. Whether you're traveling, learning a new language, or connecting with friends across the globe, we've got you covered. No more misunderstandings—experience seamless communication, anywhere, anytime.",
-            link: 'https://github.com/Ryan3389/language-translation-app'
+            heading: "BookQuest",
+            image: BookQuest,
+            description: "BookQuest is a simple an efficient is a simple and efficient CRUD application that allows users to search for books, and browse through different genres.",
+            link: 'https://github.com/Ryan3389/Book-Explorer'
         },
         {
-            heading: 'SQL Company Manager',
-            image: sqlCompanyManager,
-            description: "A command-line application for managing a company's departments, roles, and employees using a SQL database. Users can view all departments, roles, and employees, as well as add new departments, roles, and employees. Additionally, the app allows users to update employee roles, streamlining company organization and employee management.",
-            link: "https://github.com/Ryan3389/sql-company-manager"
+            heading: 'NewsHub',
+            image: NewsHub,
+            description: "NewsHub is a simple CRUD application, powered by the news api, allowing users to browse different news articles in different categories.",
+            link: "https://github.com/Ryan3389/news"
         },
         {
-            heading: 'Social Network API',
-            image: socialNetworkApi,
-            description: "A backend API for a social networking platform, allowing users to create profiles, post comments.",
-            link: "https://github.com/Ryan3389/Social-Network-Api"
+            heading: 'TastyTable',
+            image: TastyTable,
+            description: "TastyTable is a simple CRUD application, powered by theMealDB api, allowing users to browse different recipes in different categories.",
+            link: "https://github.com/Ryan3389/TastyTable"
         },
     ]);
 
