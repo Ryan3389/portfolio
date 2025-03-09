0a7ddee0e89fa9ed0497b53007829ac16f2b26b8
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-03-08 19:53:16 -0500

    refactored styles for responsive UI

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import BtnComponent from '../components/BtnComponent'
 
 const HomePage = () => {
     return (
-        <section className="bg-backgroundColor text-secondaryColor ">
+        <section className="bg-backgroundColor text-secondaryColor hero-section">
             <div className="grid max-w-screen-xl px-4 py-8 mx:auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 m-auto w-[80%]">
                 <div className="mr-auto place-self-center lg:col-span-7">
                     <h1 className="max-w-2xl mb-4 text-4x font-extrabold l md:text-5xl text-center">Full-Stack Developer</h1>
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -6,7 +6,7 @@ const ProjectsPage = () => {
             <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
                 <article className="mb-10">
                     <h2 className="text-center mb-4 text-4xl  font-extrabold">Full Stack Developer</h2>
-                    <p className="sm:text-xl text-secondaryText">I am a full-stack developer specializing in modern technologies. I create dynamic, user-friendly web applications that transform ideas into seamless, functional solutions.</p>
+                    <p className="sm:text-xl text-secondaryText projects-intro">I am a full-stack developer specializing in modern technologies. I create dynamic, user-friendly web applications that transform ideas into seamless, functional solutions.</p>
                 </article>
                 <ProjectsComponent />
             </div>
