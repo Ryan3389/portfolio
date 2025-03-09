17ea5aefcd75330b4581c8660bfd46e6f965c08b
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-03-08 19:23:51 -0500

    updated projects and resume section styles

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -5,7 +5,7 @@ import resume from '../assets/resume.pdf'
 const ResumePage = () => {
 
     return (
-        <section className="bg-backgroundColor text-secondaryColor h-[90vh]">
+        <section className="bg-backgroundColor text-secondaryColor h-[90vh] resume-section">
             <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
                 <article className="flex flex-col items-center m-auto max-w-screen-md mb-8 lg:mb-16">
                     <h2 className="mb-4 text-4xl  font-extrabold">Download my Resume</h2>
